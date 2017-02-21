1ee88301af4a296268dc69f4692d40d787bf0727
Jeremy Clark <jjclark@wellington.com>
2017-02-20 21:39:51 -0500

    Updates to spot checking. Looks like gradientBoostingRegression is the winner of all those that I tried.

--- python-mini-course/spot-checking/spot-check.py
+++ python-mini-course/spot-checking/spot-check.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsRegressor
 import sklearn.svm as svm
+from sklearn import tree
+from sklearn import ensemble
 import sklearn.linear_model as lm
 url = "https://goo.gl/sXleFv"
 names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
@@ -20,8 +22,13 @@ omp = lm.OrthogonalMatchingPursuit()
 br = lm.BayesianRidge()
 kn = KNeighborsRegressor()
 svr = svm.SVR()
-scoring = 'neg_mean_squared_error'
-models = [linregression, ridge, lasso, lars, omp, br, kn, svr]
+dtr = tree.DecisionTreeRegressor()
+rfr = ensemble.RandomForestRegressor()
+gbr = ensemble.GradientBoostingRegressor()
+mse = 'neg_mean_squared_error'
+r2 = 'r2'
+models = [linregression, ridge, lasso, lars, omp, br, kn, svr, dtr, rfr, gbr]
 for model in models:
-    results = cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
-    print('%s: %.3f') % (model.__class__, results.mean())
+    mseResult = cross_val_score(model, X, Y, cv=kfold, scoring=mse)
+    r2result = cross_val_score(model, X, Y, cv=kfold, scoring=r2)
+    print('%s: MSE: %.3f, R2: %.3f') % (model.__class__, mseResult.mean(), r2result.mean())
